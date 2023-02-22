b6dfe80a69c93b1846dd2db3eb3a59ebcc675f67
E-David-O <124388562+E-David-O@users.noreply.github.com>
2023-02-22 17:09:23 +0000

    Add Email Adapter
    Simple Email adapter to pull the email adress from the HTML form using spring boot. Includes everything for a simple demo where upon entering e-mail adress it returns a simple confirmation note.
    
    Makes use of a slightly modified PluggableAuth.html to include thymeleaf and it style.

---
+++ PluggableAuth/src/main/java/com/Group25/PluggableAuth/PluggableAuthApplication.java
@@ -0,0 +1,13 @@
+package com.Group25.PluggableAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluggableAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluggableAuthApplication.class, args);
+	}
+
+}
