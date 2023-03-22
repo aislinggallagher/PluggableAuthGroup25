dd94cc46c0f07fbf3ae48c2a0d1bf71b546d249c
ying9999 <99326187+ying9999@users.noreply.github.com>
2023-03-22 03:00:48 +0000

    dummy webpage

--- dummy-site/src/components/HelloWorld.vue
+++ dummy-site/src/components/HelloWorld.vue
@@ -1,14 +1,21 @@
 <template>
-  <router-view />
+  <router-view /> 
+              <nav>
+                <div class="logo">Pluggable Website</div>
+                    <div class="li"><a href="#">HOME</a></div>
+                    <div class="li"><a href="#">ABOUT</a></div>
+                    <div class="li"><a href="#">SERVICE</a></div>
+                    <div class="li"><a href="#">CONTACT</a></div>
+              </nav>
   <main>
   <v-app>
     <div class="background"></div>
     <v-main class="d-flex justify-center align-center">
-      <v-col cols="10" lg="4" class="mx-auto">
+      <v-col cols="7" lg="4" class="mx-auto">
         <v-card>
           <div class="text-center">
             <v-avatar size="100" color="indigo lighten-4">
-              <v-icon size="40" color="white">mdi-account</v-icon>
+              <v-icon size="50" color="white">mdi-account</v-icon>
             </v-avatar>
             <h2 class="indigo--text">Pluggable Auth Module Login</h2>
           </div>
@@ -51,4 +58,54 @@ position: absolute;
 top: 0;
 background-size: cover
 } 
+</style>
+
+
+<style>
+nav{
+    background-color: #494646;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+nav .logo{
+    color: #ff7200;
+    font-size: 35px;
+    font-family: Arial;
+    padding-left: 10px;
+    float: left;
+    padding-top: 10px;
+}
+
+nav .li{
+    color: #fff;
+    padding: 10px 20px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+}
+
+nav .li a{
+    text-decoration: none;
+    color: #fff;
+    font-family: Arial;
+    font-weight: bold;
+    transition: 0.4s ease-in-out;
+}
+
+nav .li.active,
+nav .li:hover {
+  border-bottom-color: #7faeeb;
+  transition: 0.4s ease-in-out;
+}
+
+nav .li a:hover{
+    color: #40B5AD;
+    text-decoration: none;
+    transition: 0.4s ease-in-out;
+}
+
 </style>
