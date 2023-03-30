1e9a41c2021a6a72e19779aed5c5ff331078a360
Canonyai <99308100+Canonyai@users.noreply.github.com>
2023-03-30 16:04:03 +0100

    Update OutboundAdapterConfig.java

--- PluggableAuth/src/main/PluggableAuth/Adapters/outbound/OutboundAdapterConfig.java
+++ PluggableAuth/src/main/PluggableAuth/Adapters/outbound/OutboundAdapterConfig.java
@@ -1,8 +1,22 @@
 package com.Group25.PluggableAuth.Adapters.outbound;
 
+import com.Group25.PluggableAuth.Adapters.outbound.SendMail.SendMailDemo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+
+
+@Configuration
 public class OutboundAdapterConfig {
 
+    @Autowired
+    private JavaMailSenderImpl mailSender;
 
+    @Bean
+    public SendMailDemo SendMailDemo() {
+        return new SendMailDemo();
+    }
 }
