0fb684aea9ee394765e1940e3a444f8718f3ee54
Eliel Oshiokameh <oshiokae@tcd.ie>
2023-04-06 01:45:15 +0100

    moving admin portal to views and creating router link

--- admin-site/src/router/index.ts
+++ admin-site/src/router/index.ts
@@ -13,6 +13,12 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+
+      },
+      {
+        path: '',
+        name: 'dashboard',
+        component: () => import('@/views/AdminPortal.vue'),
       },
     ],
   },
